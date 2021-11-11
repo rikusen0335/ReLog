c262ad3ed7b486b99731dade43dc4ceed7026ffb
rikusen0335 <rikusen0335@gmail.com>
2021-11-12 00:01:50 +0900

    :construction: link消してみた

--- pages/api/ogp/[title].tsx
+++ pages/api/ogp/[title].tsx
@@ -57,10 +57,6 @@ export const getServerSideProps: GetServerSideProps<Props, Params> = async (cont
 
   const html = `<html>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
-
         <style>
           body {
             width: 1200px;
