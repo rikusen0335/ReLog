5c34191234f82937f3d502c59ab3ee9a2c43733a
rikusen0335 <rikusen0335@gmail.com>
2021-11-14 23:12:15 +0900

    :lipstick: transition変えるやつわからん

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -11,11 +11,11 @@ export const ThemeButton = () => {
     const toggleTheme = () => toggle(!isDarkMode)
 
     return (
-        <div className="w-10 h-10 flex items-center justify-center rounded hover:bg-[#aaaaaa20] dark:hover:bg-[#ffffff20] dark:text-light-200 delay-[15ms] duration-150 ease-in-out cursor-pointer">
+        <div onClick={toggleTheme} className="w-10 h-10 flex items-center justify-center rounded hover:bg-[#aaaaaa20] dark:hover:bg-[#ffffff20] dark:text-light-200 delay-[15ms] duration-150 ease-in-out cursor-pointer">
             {isDarkMode ? (
-                <MoonIcon onClick={toggleTheme} />
+                <MoonIcon />
             ) : (
-                <SunIcon onClick={toggleTheme} />
+                <SunIcon />
             )}
         </div>
     )
