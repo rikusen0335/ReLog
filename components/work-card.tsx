8c4b44dfd743a50a3e7937881b2047ebb5f091c7
rikusen0335 <rikusen0335@gmail.com>
2022-02-15 01:04:14 +0900

    :iphone: Show text initially for touch devices

--- components/work-card.tsx
+++ components/work-card.tsx
@@ -10,12 +10,12 @@ export const WorkCard: VFC<Prop> = ({ work: { slug, title, subtitle, introductio
     return (
         <Link href={`/works/${slug}`}>
             <div className="relative h-[500px] dark:brightness-[90%] group hover:cursor-pointer transition duration-150">
-                <img src={thumbnail} className="h-[500px] w-full object-cover group-hover:brightness-[30%] group-hover:grayscale-[0.4] ease-out transition duration-150" />
-                <div className="absolute inset-0 flex-col hidden p-16 transition duration-150 group-hover:flex">
-                    <p className="text-4xl font-bold text-light-100">{title}</p>
-                    <p className="mt-1 text-xl font-bold text-light-200">{subtitle}</p>
+                <img src={thumbnail} className="h-[500px] w-full object-cover brightness-[30%] lg:brightness-[90%] group-hover:brightness-[30%] group-hover:grayscale-[0.4] ease-out transition duration-150" />
+                <div className="absolute inset-0 flex flex-col p-16 transition duration-150 lg:hidden group-hover:flex">
+                    <h2 className="text-3xl font-bold lg:text-4xl text-light-100">{title}</h2>
+                    <h3 className="mt-1 text-xl font-bold text-light-200">{subtitle}</h3>
                     <p className="mt-6 text-light-100">{introduction}</p>
-                    <div className="flex mt-4 space-x-4">
+                    <div className="flex flex-wrap mt-4 space-x-4">
                         {used.map((name, idx) => <p key={idx} className="text-light-300">{name}</p>)}
                     </div>
                     <p className="mt-auto text-lg font-bold text-center text-light-300">クリックしてもっと見る</p>
