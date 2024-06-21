92994d220437260244502aa32ec616af279ea7af
Soshi Homma <rikusen0335@gmail.com>
2024-06-21 19:43:35 +0900

    wip: app directory

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,10 +1,10 @@
 // contentlayer.config.ts
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, defineNestedType, makeSource } from "contentlayer/source-files";
 
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeCodeTitles from "rehype-code-titles";
-import rehypeMdxCodeProps from 'rehype-mdx-code-props'
-import rehypeMdxImportMedia from 'rehype-mdx-import-media'
+import rehypeMdxCodeProps from "rehype-mdx-code-props";
+import rehypeMdxImportMedia from "rehype-mdx-import-media";
 import rehypePrismPlus from "rehype-prism-plus";
 import rehypeSlug from "rehype-slug";
 import remarkBreaks from "remark-breaks";
@@ -12,34 +12,59 @@ import remarkDirective from "remark-directive";
 import remarkDirectiveRehype from "remark-directive-rehype";
 // import rehypeShiki from "@leafac/rehype-shiki";
 import remarkGfm from "remark-gfm";
-import { remarkMdxCodeMeta } from "remark-mdx-code-meta";
-import { remarkMdxImages } from "remark-mdx-images";
 
-export const Post = defineDocumentType(() => ({
-  name: 'Post',
-  filePathPattern: "**/*.md",
+const Author = defineNestedType(() => ({
+  name: 'Author',
   fields: {
-    title: { type: 'string', required: true },
-    date: { type: 'date', required: true },
-  },
-  computedFields: {
-    url: { type: 'string', resolve: (post) => `/posts/${post._raw.flattenedPath}` },
+    name: {
+      type: 'string',
+      required: true,
+    },
+    avatarPath: {
+      type: 'string',
+      required: true,
+    },
   },
 }))
 
-export default makeSource({ contentDirPath: 'posts', documentTypes: [Post], mdx: { remarkPlugins: [
-  // remarkHint,
-  remarkBreaks,
-  remarkGfm,
-  remarkMdxImages,
-  remarkDirective,
-  remarkDirectiveRehype,
-], rehypePlugins: [
-  rehypeSlug,
-  rehypeCodeTitles,
-  rehypePrismPlus,
-  rehypeAutolinkHeadings,
-  // rehypeMdxImportMedia,
-  // rehypeMdxCodeProps,
-]}})
-  // remarkMdxCodeMeta,]} })
+export const Post = defineDocumentType(() => ({
+	name: "Post",
+	filePathPattern: "**/*.md",
+	fields: {
+    slug: { type: "string", required: true },
+		title: { type: "string", required: true },
+		publishedDate: { type: "date", required: true },
+    author: { type: "nested", required: true, of: Author },
+    excerpt: { type: "string", required: true },
+    tags: { type: "list", of: { type: "string" } },
+    public: { type: "boolean" },
+	},
+	computedFields: {
+		url: {
+			type: "string",
+			resolve: (post) => `/posts/${post._raw.flattenedPath}`,
+		},
+	},
+}));
+
+export default makeSource({
+	contentDirPath: "posts",
+	documentTypes: [Post],
+	mdx: {
+		remarkPlugins: [
+			// remarkHint,
+			remarkBreaks,
+			remarkGfm,
+			remarkDirective,
+			remarkDirectiveRehype,
+		],
+		rehypePlugins: [
+			rehypeSlug,
+			rehypeCodeTitles,
+			rehypePrismPlus,
+			rehypeAutolinkHeadings,
+			rehypeMdxImportMedia,
+			rehypeMdxCodeProps,
+		],
+	},
+});
